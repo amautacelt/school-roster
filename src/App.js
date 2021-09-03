10ed866745573190a9da7518bb43851d7df04c34
amautacelt <amautacelt@gmail.com>
2021-09-03 17:20:37 -0400

    table and forms

--- src/App.js
+++ src/App.js
@@ -11,13 +11,56 @@ function App() {
         <div className="search-and-filter">
           <form className="search">
             <input type="text" name="search-term" />
-            <i>Icon</i>
+            <i className="fa fa-search"></i>
           </form>
           <form className="filter">
+            <select>
+              <option value="all">Show all</option>
+              <option value="students">Show only students</option>
+              <option value="teachers">Show only teachers</option>
+              <option value="administrators">Show only admins</option>
+            </select>
           </form>
-
         </div>
       </header>
+
+      <main>
+        <table className="people-table">
+          <thead>
+            <tr>
+              <th>First Name <button> <i className="fa fa-caret-down"> </i> </button> </th>
+              <th>Last Name <button> <i className="fa fa-caret-down"> </i> </button> </th>
+              <th>Role <button> <i className="fa fa-caret-down"> </i> </button> </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Kyle</td>
+              <td>Coberly</td>
+              <td>Teacher</td>
+            </tr>
+            <tr>
+              <td>Brian</td>
+              <td>Firooz</td>
+              <td>Administrator</td>
+            </tr>
+            
+          </tbody>
+        </table>
+
+        <form className="add-new">
+          <h2>Add New Person</h2>
+          <input required type="text" name="firstName" placeholder="First Name" />
+          <input required type="text" name="lastName" placeholder="Last Name" />
+          <select required name="role">
+            <option value="student">Student</option>
+            <option value="teacher">Teacher</option>
+            <option value="administrator">Administrator</option>
+          </select>
+          <input type="submit" value="Add Person" />
+        </form>
+
+      </main>
     </div>
   );
 }
